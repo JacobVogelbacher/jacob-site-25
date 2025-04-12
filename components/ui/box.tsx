ecd8ae00216ee6b88bf90d66cec915e06d29741b
Jacob Vogelbacher <jacob@Jacobs-MacBook-Pro.local>
2025-04-12 16:58:18 -0400

    use border spotlight effect for the Box component

--- components/ui/box.tsx
+++ components/ui/box.tsx
@@ -1,24 +1,24 @@
-import { GlowingEffect } from '../glowing-effect'
+'use client'
+
+import { cn } from '@/lib/utils'
+import { BorderSpotlight } from '@/components/border-spotlight'
 
 export const Box = ({
   children,
+  className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div
-      className="relative flex aspect-[2/1] items-center justify-center rounded-lg border border-gray-800 bg-gray-950 p-4"
-      {...props}
-    >
-      <GlowingEffect
-        spread={40}
-        glow={true}
-        disabled={false}
-        proximity={64}
-        inactiveZone={0.01}
-        movementDuration={1}
-      />
-
-      {children}
-    </div>
+    <BorderSpotlight>
+      <div
+        className={cn(
+          'relative flex aspect-[2/1] items-center justify-center rounded-lg border border-gray-800 bg-gray-950 p-4',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    </BorderSpotlight>
   )
 }
