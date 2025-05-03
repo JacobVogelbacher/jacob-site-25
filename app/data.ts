a959d84a5758b284890476a8f96eee29c4db8274
Jacob Vogelbacher <jacob@Jacobs-MacBook-Pro.local>
2025-05-03 16:46:49 -0400

    setup data for Realty and Fracture work

--- app/data.ts
+++ app/data.ts
@@ -30,16 +30,41 @@ export const data = {
   ],
   work: [
     {
-      name: 'ORA Portal',
-      url: '#!',
+      name: 'Realty Association Portal',
+      tech: [
+        'Next.js',
+        'TypeScript',
+        'Tailwind',
+        'Supabase',
+        'Cypress',
+        'Sanity',
+        'Trigger.Dev',
+        'Storybook',
+      ],
+      description:
+        'Internal portal to simplify workflows and enhance productivity for over 20,000 members.',
+      details: ['Integrated with payment processing service.'],
+      icon: 'key' as const,
     },
     {
-      name: 'ORA Marketing',
-      url: '#!',
-    },
-    {
-      name: 'FCR',
-      url: '#!',
+      name: 'Fracture Product Customizer',
+      url: 'https://fractureme.com/gallery-walls/bella/',
+      tech: [
+        'React',
+        'Styled Components',
+        'Jest',
+        'Storybook',
+        'Parcel',
+        'Rollup',
+      ],
+      description:
+        'Interactive product customizer integrated with e-commerce platform.',
+      details: [
+        'Multi-repo project with reusable library of shared functionality.',
+        'Custom image editing logic.',
+      ],
+      icon: 'brush' as const,
+      attribution: 'Big Vision',
     },
     {
       name: 'NDL',
